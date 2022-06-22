b126c30fb72c09597f3e2cc6f0cebd0e59270355
dlqnt <delinquent.zero@protonmail.com>
2022-06-22 14:36:03 -0700

    Fix: touple typings

--- index.ts
+++ index.ts
@@ -56,7 +56,7 @@ export const extractAttributes = (
 
 export const Collections = {
   findOne: async (
-    matches: Array<[string, unknown]>,
+    matches: [string, unknown],
     collection: string,
     options?: { useMasterKey: boolean }
   ): Promise<MoralisTypes.Object | undefined> => {
@@ -70,7 +70,7 @@ export const Collections = {
       : await query.first()
   },
   findMany: async (
-    matches: Array<[string, unknown]>,
+    matches: [string, unknown],
     collection: string,
     options?: { useMasterKey: boolean }
   ): Promise<MoralisTypes.Object[]> => {
