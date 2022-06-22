1b072834c27b920df519f5c8d1cdae0eba2c1aae
dlqnt <delinquent.zero@protonmail.com>
2022-06-22 14:27:08 -0700

    Fix: updates

--- index.ts
+++ index.ts
@@ -1,37 +1,37 @@
-import MoralisTypes from "moralis/types";
-import { ACLFactoryOptions } from "./types";
+import MoralisTypes from 'moralis/types'
+import { ACLFactoryOptions } from './types'
 
 // @ts-ignore: Unreachable code error
-const Moralis = require("moralis/node");
+import Moralis from 'moralis'
+
+type MoralisStart = {
+  serverUrl: string
+  appId: string
+  masterKey: string
+}
 
 export const moralisStart = async ({
   serverUrl,
   appId,
   masterKey,
-}: {
-  serverUrl: string;
-  appId: string;
-  masterKey: string;
-}) => {
-  return await Moralis.start({ serverUrl, appId, masterKey });
-};
+}: MoralisStart) => {
+  return await Moralis.start({ serverUrl, appId, masterKey })
+}
 
 export const moralisObjectFactory = <T extends string>(
   collection: T,
   createNewInstance = false
 ) => {
-  const Obj = Moralis.Object.extends(collection);
-  return createNewInstance ? new Obj() : Obj;
-};
+  const Obj = Moralis.Object.extend(collection)
+  return createNewInstance ? new Obj() : Obj
+}
 
 export const moralisQueryFactory = <T extends string>(collection: T) => {
-  return new Moralis.Query(moralisObjectFactory(collection));
-};
+  return new Moralis.Query(moralisObjectFactory(collection))
+}
 
-export const moralisAclFactory = <T extends string>(
-  options?: ACLFactoryOptions
-) => {
-  const acl = new Moralis.ACL();
+export const moralisAclFactory = (options?: ACLFactoryOptions) => {
+  const acl = new Moralis.ACL()
 
   if (options) {
     const {
@@ -39,20 +39,20 @@ export const moralisAclFactory = <T extends string>(
       setPublicWriteAccess,
       setWriteAccess,
       setReadAccess,
-    } = options;
+    } = options
 
-    setPublicReadAccess && acl.setPublicReadAccess(setPublicReadAccess);
-    setPublicWriteAccess && acl.setPublicWriteAccess(setPublicWriteAccess);
-    setWriteAccess && acl.setWriteAccess(setWriteAccess[0], setWriteAccess[1]);
-    setReadAccess && acl.setReadAccess(setReadAccess[0], setReadAccess[1]);
+    setPublicReadAccess && acl.setPublicReadAccess(setPublicReadAccess)
+    setPublicWriteAccess && acl.setPublicWriteAccess(setPublicWriteAccess)
+    setWriteAccess && acl.setWriteAccess(setWriteAccess[0], setWriteAccess[1])
+    setReadAccess && acl.setReadAccess(setReadAccess[0], setReadAccess[1])
   }
 
-  return acl;
-};
+  return acl
+}
 
 export const extractAttributes = (
   obj: MoralisTypes.Object
-): Record<string, unknown> => obj.attributes;
+): Record<string, unknown> => obj.attributes
 
 export const Collections = {
   findOne: async (
@@ -60,55 +60,55 @@ export const Collections = {
     collection: string,
     options?: { useMasterKey: boolean }
   ): Promise<MoralisTypes.Object | undefined> => {
-    const query = moralisQueryFactory(collection);
+    const query = moralisQueryFactory(collection)
     matches.forEach(([prop, value]) => {
-      query.equalTo(prop, value);
-    });
+      query.equalTo(prop, value)
+    })
 
     return options?.useMasterKey
       ? await query.first({ useMasterKey: true })
-      : await query.first();
+      : await query.first()
   },
   findMany: async (
     matches: Array<[k: string, value: unknown]>,
     collection: string,
     options?: { useMasterKey: boolean }
   ): Promise<MoralisTypes.Object[]> => {
-    const query = moralisQueryFactory(collection);
+    const query = moralisQueryFactory(collection)
     matches.forEach(([prop, value]) => {
-      query.equalTo(prop, value);
-    });
+      query.equalTo(prop, value)
+    })
     return options?.useMasterKey
-      ? await query.find({ useMasterkey: true })
-      : await query.find();
+      ? await query.find({ useMasterkey: true } as any)
+      : await query.find()
   },
   create: async (
     data: Record<string, unknown>,
     collection: string,
     aclOptions?: ACLFactoryOptions
   ): Promise<MoralisTypes.Object> => {
-    const obj = moralisObjectFactory(collection, true);
+    const obj = moralisObjectFactory(collection, true)
     Object.entries(data).forEach(([key, value]) => {
-      obj.set(key, value);
-    });
+      obj.set(key, value)
+    })
 
     if (aclOptions) {
-      const acl = moralisAclFactory(aclOptions);
-      obj.setACL(acl);
+      const acl = moralisAclFactory(aclOptions)
+      obj.setACL(acl)
     }
 
-    return await obj.save();
+    return await obj.save()
   },
   update: async (
     updates: Record<string, unknown>,
     obj: MoralisTypes.Object,
     options?: { useMasterKey: boolean }
   ): Promise<MoralisTypes.Object> => {
-    if (updates.length === 0) return obj;
+    if (updates.length === 0) return obj
     Object.entries(updates).forEach(([key, value]) => {
-      obj.set(key, value);
-    });
-    if (options) return await obj.save(null, options);
-    return await obj.save();
+      obj.set(key, value)
+    })
+    if (options) return await obj.save(null, options)
+    return await obj.save()
   },
-} as const;
+} as const
